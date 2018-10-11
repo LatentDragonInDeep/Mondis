372f4ab0cfe107bb76ce501b800c22a46104c6df
1195673859@qq.com <csj19950623>
2018-10-11 17:24:44 +0800

    partially finished transaction

---
+++ src/MondisCluster.h
@@ -0,0 +1,14 @@
+//
+// Created by 11956 on 2018/10/11.
+//
+
+#ifndef MONDIS_MONDISCLUSTER_H
+#define MONDIS_MONDISCLUSTER_H
+
+
+class MondisCluster {
+
+};
+
+
+#endif //MONDIS_MONDISCLUSTER_H
---
+++ src/ReplicaCluster.h
@@ -0,0 +1,14 @@
+//
+// Created by 11956 on 2018/10/11.
+//
+
+#ifndef MONDIS_REPLICACLUSTER_H
+#define MONDIS_REPLICACLUSTER_H
+
+
+class ReplicaCluster {
+
+};
+
+
+#endif //MONDIS_REPLICACLUSTER_H
